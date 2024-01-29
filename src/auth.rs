43e33f94ecf7f9979963942ab35f896cff31bc37
xxmatthewandersonxx <92887765+mjanderson1227@users.noreply.github.com>
2024-01-29 16:44:46 -0600

    Made progress on oauth

--- src/auth.rs
+++ src/auth.rs
@@ -21,17 +21,16 @@ impl OAuthClient {
             TokenUrl::new("https://oauth2.googleapis.com/token".to_string())
                 .expect("Invalid token URL\n"),
         );
-        let redirect =
-            RedirectUrl::new("http://127.0.0.1:8000/books".to_string()).expect("Invalid token URL");
-
+        let redirect = RedirectUrl::new("http://127.0.0.1:8000/callback".to_string())
+            .expect("Invalid token URL");
         let client = BasicClient::new(client_id, client_secret, auth_url, token_url)
             .set_redirect_uri(redirect);
-
         Self { client }
     }
 
     pub fn gen_auth_url(self: Self) -> (Url, CsrfToken) {
-        let (pkce_challenge, _pkce_verifier) = PkceCodeChallenge::new_random_sha256();
+        let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
+        println!("verifier: {:?}\n", pkce_verifier);
         self.client
             .authorize_url(CsrfToken::new_random)
             .set_pkce_challenge(pkce_challenge)
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod auth;
 use dotenv::dotenv;
-use rocket::http::CookieJar;
+use rocket::http::{Cookie, CookieJar};
 use rocket::response::Redirect;
-use rocket::{get, launch, routes, uri, Build, Rocket};
+use rocket::{get, launch, routes, Build, Rocket};
 
 #[get("/")]
 fn root() -> &'static str {
@@ -10,26 +10,27 @@ fn root() -> &'static str {
 }
 
 #[get("/login")]
-fn login_screen(_cookies: &CookieJar<'_>) -> Redirect {
+fn login_screen(cookies: &CookieJar<'_>) -> Redirect {
     let oauth_client = auth::OAuthClient::new();
-    let (auth_url, _csrf_token) = oauth_client.gen_auth_url();
+    let (auth_url, csrf_token) = oauth_client.gen_auth_url();
+    let csrf_cookie = Cookie::build(("oauth_csrf", csrf_token.secret().to_owned()))
+        .path("/callback")
+        .build();
+    cookies.add(csrf_cookie);
     Redirect::to(auth_url.to_string())
 }
 
-#[get("/books")]
-fn books_menu(cookies: &CookieJar) {}
+#[get("/callback?<state>&<code>")]
+fn login_handler(state: String, code: String, cookies: &CookieJar<'_>) -> String {
+    format!(
+        "csrf: {}\nauth_key: {}\nCookies: {:?}",
+        state, code, cookies
+    )
+}
 
-/*
-* Some guidelines:
-* 1. I want an interface such that when the user logs in, they can see the books they have created.
-*  - Query the database and find the user info and serve it to them.
-* 2. I want to be able to reroute a user to the login screen if they are not already logged in.
-*  - Add a guard such that when the user does not meet the logged in criteria they are rerouted to
-*  the login screen
-* */
 #[launch]
 fn rocket() -> Rocket<Build> {
     // Parse the .env file
     dotenv().ok();
-    rocket::build().mount("/", routes![root, login_screen])
+    rocket::build().mount("/", routes![root, login_screen, login_handler])
 }
