540a7614232243957ae8641b35cad141a19464cf
xxmatthewandersonxx <92887765+mjanderson1227@users.noreply.github.com>
2024-02-03 15:50:02 -0600

    Added token validation with google API

--- src/auth.rs
+++ src/auth.rs
@@ -1,9 +1,20 @@
 use oauth2::basic::BasicClient;
+use oauth2::reqwest::async_http_client;
 use oauth2::url::Url;
 use oauth2::{
-    AuthUrl, ClientId, ClientSecret, CsrfToken, PkceCodeChallenge, RedirectUrl, Scope, TokenUrl,
+    AccessToken, AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, PkceCodeChallenge,
+    PkceCodeVerifier, RedirectUrl, Scope, TokenResponse, TokenUrl,
 };
 use std::env::var;
+
+#[derive(Debug, Clone)]
+pub struct TokenNotFoundError;
+
+impl std::fmt::Display for TokenNotFoundError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "A token was not found.\n")
+    }
+}
 
 pub struct OAuthClient {
     client: BasicClient,
@@ -31,15 +42,34 @@ impl OAuthClient {
         Self { client }
     }
 
-    pub fn gen_auth_url(self: Self) -> (Url, CsrfToken) {
+    pub fn gen_auth_url(&self) -> (Url, CsrfToken, PkceCodeVerifier) {
         let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
-        println!("verifier: {:?}\n", pkce_verifier);
-        self.client
+        let (link, csrf) = self
+            .client
             .authorize_url(CsrfToken::new_random)
             .set_pkce_challenge(pkce_challenge)
             .add_scope(Scope::new(
                 "https://www.googleapis.com/auth/cloud-platform.read-only".to_string(),
             ))
-            .url()
+            .url();
+        (link, csrf, pkce_verifier)
+    }
+
+    pub async fn verify_token(
+        &self,
+        token: &String,
+        verifier: &String,
+    ) -> Result<AccessToken, TokenNotFoundError> {
+        let response_result = self
+            .client
+            .exchange_code(AuthorizationCode::new(token.to_string()))
+            .set_pkce_verifier(PkceCodeVerifier::new(verifier.to_string()))
+            .request_async(async_http_client)
+            .await;
+        if let Ok(res) = response_result {
+            Ok(res.access_token().to_owned())
+        } else {
+            Err(TokenNotFoundError)
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod auth;
 use dotenv::dotenv;
 use rocket::http::{Cookie, CookieJar, SameSite};
 use rocket::response::Redirect;
-use rocket::{get, launch, routes, Build, Rocket};
+use rocket::{get, launch, routes, Build, Rocket, State};
 
 #[get("/")]
 fn root() -> &'static str {
@@ -10,22 +10,27 @@ fn root() -> &'static str {
 }
 
 #[get("/login")]
-fn login_screen(cookies: &CookieJar<'_>) -> Redirect {
-    let oauth_client = auth::OAuthClient::new();
-    let (auth_url, csrf_token) = oauth_client.gen_auth_url();
+fn login_screen(oauth_client: &State<auth::OAuthClient>, cookies: &CookieJar<'_>) -> Redirect {
+    let (auth_url, csrf_token, pkce_verifier) = oauth_client.gen_auth_url();
     /* TODO: Fix this later as it could be unsafe. (Eventually move over to using private cookies)*/
-    let csrf_cookie = Cookie::build(("oauth_csrf", csrf_token.secret().to_string()))
+    let csrf_cookie = Cookie::build(("oauth_csrf", csrf_token.secret().to_owned()))
         .path("/callback")
         .same_site(SameSite::Lax)
         .build();
+    let pkce_cookie = Cookie::build(("oauth_pkce", pkce_verifier.secret().to_owned()))
+        .same_site(SameSite::Lax)
+        .build();
     cookies.add(csrf_cookie);
+    cookies.add(pkce_cookie);
     Redirect::to(auth_url.to_string())
 }
 
 #[get("/callback?<state>&<code>")]
 fn login_handler(state: String, code: String, cookies: &CookieJar<'_>) -> Redirect {
-    let cookie_result = cookies.get("oauth_csrf");
-    if cookie_result.is_some_and(|cookie| cookie.value().to_string() == state) {
+    if cookies
+        .get("oauth_csrf")
+        .is_some_and(|cookie| cookie.value().to_string() == state)
+    {
         cookies.add(
             // TODO: Move this over to a private cookie.
             Cookie::build(("oauth_token", code))
@@ -38,10 +43,39 @@ fn login_handler(state: String, code: String, cookies: &CookieJar<'_>) -> Redire
     }
 }
 
+#[get("/authenticate")]
+async fn authenticate(
+    oauth_client: &State<auth::OAuthClient>,
+    cookies: &CookieJar<'_>,
+) -> Redirect {
+    let cookie_list = vec![cookies.get("oauth_pkce"), cookies.get("oauth_token")];
+    let strs: Vec<Option<String>> = cookie_list
+        .iter()
+        .map(|cookie| cookie.map(|coo| coo.value().to_string()))
+        .collect();
+
+    if let (Some(pkce), Some(token)) = (&strs[0], &strs[1]) {
+        let auth_token = oauth_client.verify_token(token, pkce).await;
+        match auth_token {
+            Ok(token) => {
+                cookies
+                    .add(Cookie::build(("oauth_access_token", token.secret().to_string())).build());
+                Redirect::to("/final")
+            }
+            Err(_) => Redirect::to("/login"),
+        }
+    } else {
+        Redirect::to("/login")
+    }
+}
+
 #[launch]
 fn rocket() -> Rocket<Build> {
     // Parse the .env file.
     dotenv().ok();
     // Launch the server.
-    rocket::build().mount("/", routes![root, login_screen, login_handler])
+    rocket::build().manage(auth::OAuthClient::new()).mount(
+        "/",
+        routes![root, login_screen, login_handler, authenticate],
+    )
 }
